ebe1e91ef12d21cea5af54aa5f7aa8bf2c38c03b
Christian Piehl <piehli@gmx.de>
2017-11-06 22:04:22 +0100

    changes

--- src/main/java/jnatest/Main.java
+++ src/main/java/jnatest/Main.java
@@ -1,9 +1,35 @@
 package jnatest;
 
+import com.sun.jna.Native;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("done");
+		testPosixLibrary();
+		// testKernel32Library();
+	}
+
+	private static void testPosixLibrary() {
+		PosixLibrary posix = (PosixLibrary) Native.loadLibrary("c", PosixLibrary.class);
+		System.out.println(posix.getpid());
+		System.out.println(posix.getppid());
+		try {
+			Thread.sleep(60000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		// posix.mkdir("newdir", 0777);
+		// posix.rename("newdir", "renamedir");
+	}
+
+	private static void testKernel32Library() {
+		Kernel32Library lib = (Kernel32Library) Native.loadLibrary("kernel32", Kernel32Library.class);
+		lib.Beep(698, 500);
+		lib.Sleep(500);
+		for (int i = 100; i < 1000; i = i + 40) {
+			System.out.println(i);
+			lib.Beep(i, 500);
+		}
 	}
 
 }
